160667b4c7194c4f4ba11b7444fd9966c23893e7
j-adenilson <j.adenilson@outlook.com.br>
2025-05-07 23:33:11 -0300

    first commit

---
+++ src/test/java/br/com/crud/CadastroDeFuncionarios/CadastroDeFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crud.CadastroDeFuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
