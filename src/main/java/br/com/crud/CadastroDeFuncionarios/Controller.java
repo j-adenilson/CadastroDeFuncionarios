566330fa0559b6597bdc285d8030b373afcf6ed8
j-adenilson <j.adenilson@outlook.com.br>
2025-05-08 22:53:25 -0300

    Criando a primeira rota /boavindas

---
+++ src/main/java/br/com/crud/CadastroDeFuncionarios/Controller.java
@@ -0,0 +1,16 @@
+package br.com.crud.CadastroDeFuncionarios;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+
+    @GetMapping("/boasvindas")
+    public String boasVinads(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+}
