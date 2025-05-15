829cca4f623da3fbaf7372ca2bbc345257497f1d
j-adenilson <j.adenilson@outlook.com.br>
2025-05-14 22:42:56 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/br/com/crud/CadastroDeFuncionarios/Funcionarios/FuncionariosModel.java
+++ src/main/java/br/com/crud/CadastroDeFuncionarios/Funcionarios/FuncionariosModel.java
@@ -2,11 +2,18 @@ package br.com.crud.CadastroDeFuncionarios.Funcionarios;
 
 import br.com.crud.CadastroDeFuncionarios.Setor.SetorModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //Entity transforma uma classe em entidade do banco de dados
 //JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor //Cria os construtores sem argumentos
+@AllArgsConstructor //Cria os construtores com todos os arguentos
+@Data //Cria todos os getter and setter
+
 public class FuncionariosModel {
 
     @Id
@@ -24,40 +31,7 @@ public class FuncionariosModel {
     @ManyToOne
     @JoinColumn(name = "setor_id") //Foreing Key ou chave estrangeira
     private SetorModel setor;
-
-
-
-    public FuncionariosModel() {
-    }
-
-    public FuncionariosModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
+
+
+
--- src/main/java/br/com/crud/CadastroDeFuncionarios/Setor/SetorModel.java
+++ src/main/java/br/com/crud/CadastroDeFuncionarios/Setor/SetorModel.java
@@ -2,11 +2,18 @@ package br.com.crud.CadastroDeFuncionarios.Setor;
 
 import br.com.crud.CadastroDeFuncionarios.Funcionarios.FuncionariosModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_setor")
+@NoArgsConstructor //Cria os construtores sem argumentos
+@AllArgsConstructor //Cria os construtores com todos os arguentos
+@Data //Cria todos os getter and setter
+
 public class SetorModel {
 
     @Id
