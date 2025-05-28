c7c09650bbdd0efef608c521016e1b8ae23a4e43
j-adenilson <j.adenilson@outlook.com.br>
2025-05-28 20:31:43 -0300

    adiciona annotations, `Postmapping`, `Getmapping`, `Putmapping`,`Deletemapping

--- src/main/java/br/com/crud/CadastroDeFuncionarios/Funcionarios/FuncionariosController.java
+++ src/main/java/br/com/crud/CadastroDeFuncionarios/Funcionarios/FuncionariosController.java
@@ -1,7 +1,5 @@
 package br.com.crud.CadastroDeFuncionarios.Funcionarios;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -13,4 +11,32 @@ public class FuncionariosController {
         return "Essa é minha primeira mensagem nessa rota";
     }
 
+
+    //Adicionar funconarios  (CREATE)
+    @PostMapping("/criar")
+    public String criarfuncionario(){
+        return "Funcionário criado";
+    }
+
+    //Procurar funcionarios por ID (REATE)
+    @GetMapping("/todos")
+    public String mostrarTodosOsfuncionariosId(){
+        return "Mostrar Funcionarios";
+    }
+
+    //Mostrar os funcionarios por ID (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsfuncionarios(){
+        return "Mostrar Funcionarios Por ID";
+    }
+    //Alterar dados dos funcionarios (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarFuncionarioPorId(){
+        return "Alterar Funcionarios Por ID";
+    }
+    //Deletar funcionarios (DELETE)
+    @DeleteMapping("/deletarID")
+    public  String deletarPorId(){
+        return "Deletar por ID";
+    }
 }
